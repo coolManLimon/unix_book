0e7fc8e15f8953d932b0f5e1c8b53c46cde817f4
coolManLimon <mr_cZhaoPeng@163.com>
2021-05-24 05:36:52 -0700

    all_done

--- cap4/count_file_num.cpp
+++ cap4/count_file_num.cpp
@@ -9,6 +9,7 @@
 
 
 static long lnk, dir, file, socket, fifo, blk, chr, tot;
+static std::string full_name;
 
 int myFtw(std::string pathName);
 void doIt(std::string pathName, int type, const struct stat *stat_ptr);
@@ -23,6 +24,7 @@ void doIt(std::string pathName, int type, const struct stat *stat_ptr)
     case F_FILE:
         switch (stat_ptr->st_mode & S_IFMT)
         {
+            case S_IFREG :  file++;   break;
             case S_IFSOCK:  socket++;   break;
             case S_IFBLK:   blk++;      break;
             case S_IFCHR:   chr++;      break;
@@ -31,7 +33,7 @@ void doIt(std::string pathName, int type, const struct stat *stat_ptr)
             default:                    break;
         }
     default:
-        std::cout << "can't read dir:" << pathName << std::endl;
+        //std::cout << "can't read dir:" << pathName << std::endl;
         break;
     }
 }
@@ -50,13 +52,11 @@ int myFtw(std::string pathName)
     }
     if (S_ISDIR(stat_buf.st_mode) == 0)     // 是文件而非目录
     {
-        std::cout << "   is file" << std::endl;
         doIt(pathName, F_FILE, &stat_buf);
         return 0;
     }
 
     // 目录
-    std::cout << pathName << "   is dir" << std::endl;
     doIt(pathName, F_DIR, &stat_buf);
     if ((dp = opendir(pathName.c_str())) != NULL)
     {
@@ -68,17 +68,12 @@ int myFtw(std::string pathName)
             }
             else
             {
-                std::string tmp = pathName.append("/").append(dir->d_name);
-                myFtw(tmp);
-                std::cout << "cccccccccccc" << std::endl;
-                return 1;
+                std::string tmp = pathName;                 // 注意：append()函数会修改原字符串的值
+                myFtw(tmp.append("/").append(dir->d_name));
             }
         }
-        std::cout << "zzzzzzzzzzzzzzz" << std::endl;
         return 1;
     }
-    std::cout << "ppppppppp" << std::endl;
-    return 1;
 }
 
 int main(int argc, char **argv)
@@ -90,14 +85,16 @@ int main(int argc, char **argv)
         std::cout << "cannot use as this way!\n";
     }
 
+    full_name = argv[1];
+
     ret = myFtw(argv[1]);
     tot = lnk + dir + file + socket + fifo + blk + chr;
 
-    printf("link num = %7ld, %5.2f %%\n", lnk, lnk * 100.0 / tot);
-    printf("dir num = %7ld, %5.2f %%\n", dir, dir * 100.0 / tot);
-    printf("file num = %7ld, %5.2f %%\n", file, file * 100.0 / tot);
-    printf("socket num = %7ld, %5.2f %%\n", socket, socket * 100.0 / tot);
-    printf("fifo num = %7ld, %5.2f %%\n", fifo, fifo * 100.0 / tot);
-    printf("blk num = %7ld, %5.2f %%\n", blk, blk * 100.0 / tot);
-    printf("chr num = %7ld, %5.2f %%\n", chr, chr * 100.0 / tot);
+    printf("link num \t= \t%7ld,\t %5.2f %%\n", lnk, lnk * 100.0 / tot);
+    printf("dir num \t= \t%7ld,\t %5.2f %%\n", dir, dir * 100.0 / tot);
+    printf("file num \t= \t%7ld,\t %5.2f %%\n", file, file * 100.0 / tot);
+    printf("socket num \t= \t%7ld,\t %5.2f %%\n", socket, socket * 100.0 / tot);
+    printf("fifo num \t= \t%7ld,\t %5.2f %%\n", fifo, fifo * 100.0 / tot);
+    printf("blk num \t= \t%7ld,\t %5.2f %%\n", blk, blk * 100.0 / tot);
+    printf("chr num \t= \t%7ld,\t %5.2f %%\n", chr, chr * 100.0 / tot);
 }
